4e0772946c4d4fe90dcce2fff3c129fd7f64d9cb
Poohboss <13412101248@163.com>
2021-03-05 22:30:23 +0800

    发布页面加了上传图片的监听

--- app/src/main/java/com/example/yilaoapp/ui/purchase/PurchaseMessageFragment.java
+++ app/src/main/java/com/example/yilaoapp/ui/purchase/PurchaseMessageFragment.java
@@ -1,9 +1,12 @@
 package com.example.yilaoapp.ui.purchase;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Point;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.databinding.DataBindingUtil;
@@ -11,22 +14,40 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.yilaoapp.R;
 import com.example.yilaoapp.databinding.FragmentPurchaseMessageBinding;
 
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+
+import cn.bingoogolapple.photopicker.activity.BGAPhotoPickerActivity;
+import cn.bingoogolapple.photopicker.activity.BGAPhotoPickerPreviewActivity;
+import cn.bingoogolapple.photopicker.widget.BGASortableNinePhotoLayout;
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class PurchaseMessageFragment extends Fragment {
+public class PurchaseMessageFragment extends Fragment implements EasyPermissions.PermissionCallbacks, BGASortableNinePhotoLayout.Delegate{
+
+    private static final int PRC_PHOTO_PICKER = 1;
+    private static final int RC_CHOOSE_PHOTO = 1;
+    private static final int RC_PHOTO_PREVIEW = 2;
+    private static final String EXTRA_MOMENT = "EXTRA_MOMENT";
 
     public PurchaseMessageFragment() {}
 
@@ -50,14 +71,88 @@ public class PurchaseMessageFragment extends Fragment {
                 controller.popBackStack();
             }
         });
-        Point p = new Point();//获取窗口管理器
-        WindowManager wm = (WindowManager) container.getContext().getSystemService(Context.WINDOW_SERVICE);
-        wm.getDefaultDisplay().getSize(p);
-        int screenWidth = p.x; // 屏幕宽度
-        LinearLayout.LayoutParams lp=new LinearLayout.LayoutParams(200,200);
-        lp.setMargins(20,20,screenWidth/3,20);
-        binding.addPhoto.setLayoutParams(lp);
+        binding.mPurchasePhotosSnpl.setMaxItemCount(9);
+        binding.mPurchasePhotosSnpl.setEditable(true);
+        binding.mPurchasePhotosSnpl.setPlusEnable(true);
+        binding.mPurchasePhotosSnpl.setSortable(true);
+        binding.mPurchasePhotosSnpl.setDelegate((BGASortableNinePhotoLayout.Delegate) this);
+
         return binding.getRoot();
-        //return inflater.inflate(R.layout.fragment_purchase_message, container, false);
+        //return inflater.inflate(R.layout.fragment_bullentin_message, container, false);
+    }
+
+
+    @AfterPermissionGranted(PRC_PHOTO_PICKER)
+    private void choicePhotoWrapper() {
+        String[] perms = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA};
+        if (EasyPermissions.hasPermissions(requireContext(), perms)) {
+            // 拍照后照片的存放目录，改成你自己拍照后要存放照片的目录。如果不传递该参数的话就没有拍照功能
+            File takePhotoDir = new File(Environment.getExternalStorageDirectory(), "BGAPhotoPickerTakePhoto");
+
+            Intent photoPickerIntent = new BGAPhotoPickerActivity.IntentBuilder(requireContext())
+                    .cameraFileDir(null) // 拍照后照片的存放目录，改成你自己拍照后要存放照片的目录。如果不传递该参数的话则不开启图库里的拍照功能
+                    .maxChooseCount(
+                            binding.mPurchasePhotosSnpl.getMaxItemCount() - binding.mPurchasePhotosSnpl.getItemCount()) // 图片选择张数的最大值
+                    .selectedPhotos(null) // 当前已选中的图片路径集合
+                    .pauseOnScroll(false) // 滚动列表时是否暂停加载图片
+                    .build();
+            startActivityForResult(photoPickerIntent, RC_CHOOSE_PHOTO);
+        } else {
+            EasyPermissions.requestPermissions(this, "图片选择需要以下权限:\n\n1.访问设备上的照片\n\n2.拍照", PRC_PHOTO_PICKER, perms);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+        if (requestCode == PRC_PHOTO_PICKER) {
+            Toast.makeText(requireContext(), "您拒绝了「图片选择」所需要的相关权限!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == RC_CHOOSE_PHOTO) {
+            binding.mPurchasePhotosSnpl.addMoreData(BGAPhotoPickerActivity.getSelectedPhotos(data));
+        } else if (requestCode == RC_PHOTO_PREVIEW) {
+            binding.mPurchasePhotosSnpl.setData(BGAPhotoPickerPreviewActivity.getSelectedPhotos(data));
+        }
+    }
+
+    @Override
+    public void onClickAddNinePhotoItem(BGASortableNinePhotoLayout sortableNinePhotoLayout, View view, int position, ArrayList<String> models) {
+        choicePhotoWrapper();
+    }
+
+    @Override
+    public void onClickDeleteNinePhotoItem(BGASortableNinePhotoLayout sortableNinePhotoLayout, View view, int position, String model, ArrayList<String> models) {
+        binding.mPurchasePhotosSnpl.removeItem(position);
+    }
+
+    @Override
+    public void onClickNinePhotoItem(BGASortableNinePhotoLayout sortableNinePhotoLayout, View view, int position, String model, ArrayList<String> models) {
+        Intent photoPickerPreviewIntent = new BGAPhotoPickerPreviewActivity.IntentBuilder(requireContext())
+                .previewPhotos(models) // 当前预览的图片路径集合
+                .selectedPhotos(models) // 当前已选中的图片路径集合
+                .maxChooseCount(binding.mPurchasePhotosSnpl.getMaxItemCount()) // 图片选择张数的最大值
+                .currentPosition(position) // 当前预览图片的索引
+                .isFromTakePhoto(false) // 是否是拍完照后跳转过来
+                .build();
+        startActivityForResult(photoPickerPreviewIntent, RC_PHOTO_PREVIEW);
+    }
+
+    @Override
+    public void onNinePhotoItemExchanged(BGASortableNinePhotoLayout sortableNinePhotoLayout, int fromPosition, int toPosition, ArrayList<String> models) {
+        Toast.makeText(requireContext(), "排序发生变化", Toast.LENGTH_SHORT).show();
     }
 }
