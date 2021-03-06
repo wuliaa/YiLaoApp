3685e56c8af7d53c3a40c68fa3661925665e98f1
jgq111 <1101916681@qq.com>
2021-03-06 11:08:19 +0800

    uuid

--- app/src/main/java/com/example/yilaoapp/service/image_service.java
+++ app/src/main/java/com/example/yilaoapp/service/image_service.java
@@ -4,6 +4,7 @@ import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
@@ -12,5 +13,6 @@ public interface image_service {
     @POST("users/{mobile}/resources")
     Call<ResponseBody> send_photo(@Path("mobile")String mobile, @Query("token")String token, @Query("appid")String app, @Body RequestBody image);
     //   RequestBody requestBody = RequestBody.create(MediaType.parse("image/jpg"), bytes);
-
+    @GET("users/{mobile}/resources/{uuid}")
+    Call<ResponseBody> load_photo(@Path("mobile")String mobile,@Path("uuid")String uuid,@Query("token")String token,@Query("appid")String appid);
 }
