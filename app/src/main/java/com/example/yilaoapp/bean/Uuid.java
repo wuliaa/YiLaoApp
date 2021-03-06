283a4a3e8207ca1bbd9d4d412bbcee028ce7fd02
jgq111 <1101916681@qq.com>
2021-03-06 10:47:30 +0800

    uuid

---
+++ app/src/main/java/com/example/yilaoapp/bean/Uuid.java
@@ -0,0 +1,17 @@
+package com.example.yilaoapp.bean;
+
+public class Uuid {
+    String uuid;
+
+    public Uuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+}
