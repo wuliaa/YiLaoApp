3e89b7e24039c49efe99f514b6e1a1efdcf5f176
jgq111 <1101916681@qq.com>
2021-03-08 21:37:13 +0800

    appid错误修正

--- app/src/main/java/com/example/yilaoapp/ui/purchase/PurchaseMessageFragment.java
+++ app/src/main/java/com/example/yilaoapp/ui/purchase/PurchaseMessageFragment.java
@@ -145,7 +145,7 @@ public class PurchaseMessageFragment extends Fragment implements EasyPermissions
                             Point_address des=new Point_address(0,0,address);
                             pur_order order=new pur_order(phone,"代购",detail,des,money,u.getUuid());
                             pur_service pur=new RetrofitUser().get().create(pur_service.class);
-                            Call<ResponseBody> new_order=pur.new_order(mobile2,token2,"\"df3b72a07a0a4fa1854a48b543690eab\"",order);
+                            Call<ResponseBody> new_order=pur.new_order(mobile2,token2,"df3b72a07a0a4fa1854a48b543690eab",order);
                             new_order.enqueue(new Callback<ResponseBody>() {
                                 @Override
                                 public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
