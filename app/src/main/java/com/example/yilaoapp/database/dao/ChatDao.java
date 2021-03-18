5e6ed7a9997838c3cf965b00bb7aa28b2947697e
wuliaa <278897944@qq.com>
2021-03-18 10:11:40 +0800

    聊天完善

--- app/src/main/java/com/example/yilaoapp/database/dao/ChatDao.java
+++ app/src/main/java/com/example/yilaoapp/database/dao/ChatDao.java
@@ -1,5 +1,6 @@
 package com.example.yilaoapp.database.dao;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
@@ -12,8 +13,11 @@ import java.util.List;
 
 @Dao
 public interface ChatDao {
-    @Query("SELECT * FROM chat_table")
-    List<Mess> getAll();
+    @Query("SELECT * FROM chat_table order by id")
+    LiveData<List<Mess>> getAll();
+
+    @Query("SELECT * FROM chat_table order by id")
+    List<Mess> getAll2();
 
     @Insert
     void insert(Mess mess);
